af2a0b7dba21f8b240d139bbbacac7226e8a57d4
yanis.hlali <yanis.hlali@gmail.com>
2024-10-08 15:16:47 +0200

    Refactor server.js to remove unused code and add missing newline at end of file

--- server.js
+++ server.js
@@ -25,11 +25,5 @@ if (require.main === module) {
   });
 }
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 
 module.exports = app;
