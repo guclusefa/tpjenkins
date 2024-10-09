c6680fe8b9441094bb789059a0540e3fc49c34c0
guclusefa <sefaaguclu@gmail.com>
2024-10-09 11:38:02 +0200

    config sonar w/jest

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   testTimeout: 20000,
+  collectCoverage: true,
+  coverageReporters: ['lcov', 'text-summary'],  // 'lcov' pour SonarCloud, 'text-summary' pour les rapports locaux
+  reporters: [
+    'default',
+    [
+      'jest-sonar-reporter',
+      {
+        outputDirectory: 'coverage', // le dossier où SonarCloud ira chercher le rapport
+        outputName: 'sonar-report.xml', // nom du fichier rapport
+      },
+    ],
+  ],
 };
