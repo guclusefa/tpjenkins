a33b5acc190ba409109d02d57ff4b79712480178
yanis.hlali <yanis.hlali@gmail.com>
2024-10-09 15:08:48 +0200

    Remove console log

--- config/db.js
+++ config/db.js
@@ -4,12 +4,6 @@ require("dotenv").config();
 let connection;
 
 if (process.env.NODE_ENV !== "test") {
-  console.log({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-  });
   connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
